34d529ab9c4c5f7ba9560f1ad783ef1708e07ef3
AyushiMalviya08 <malviya.ayushi2003@gmail.com>
2024-06-11 21:26:34 +0530

    edit vite

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['framer-motion', 'react-icons/fa']
+      external: ['framer-motion', 'react-icons']
     }
   }
 });
