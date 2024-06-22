4b08a64a1ff597a6ee353350f9a77e867b848089
AyushiMalviya08 <malviya.ayushi2003@gmail.com>
2024-06-22 11:59:45 +0530

    eddittss

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss(),],
+  plugins: [react()],
   base: '/Portfolioo/',
   build: {
     rollupOptions: {
       external: ['react-icons']
     }
-  }
+  },
+  css: {
+    postcss: {
+      plugins: [
+        autoprefixer(), // Use autoprefixer in PostCSS plugins
+      ],
+    },
+  },
 })
