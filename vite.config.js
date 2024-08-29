5d5dfed74aa45cf6f1836e08812879bf4322a413
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-08-29 22:16:30 +0530

    Update vite.config.js
    Removed base url

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Portfolioo/',
   build: {
     outDir: 'dist',
     rollupOptions: {
